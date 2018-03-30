fix data parallel computation bug

models/networks.py
@@ -489,7 +489,7 @@ class PixelDiscriminator(nn.Module):
         self.net = nn.Sequential(*self.net)
 
     def forward(self, input):
-        if len(self.gpu_ids) and isinstance(input.data, torch.cuda.FloatTensor):
+        if len(self.gpu_ids) > 1 and isinstance(input.data, torch.cuda.FloatTensor):
             return nn.parallel.data_parallel(self.net, input, self.gpu_ids)
         else:
             return self.net(input)